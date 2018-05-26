9476a7e7f00a89056cdc0d7a2414bcd242cc1dfa
Timo de Jong <timodejong95@gmail.com>
2018-05-26 20:04:50 +0200

    Fixed return type

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Response;
+use Illuminate\View\View;
 
 class HomeController extends Controller
 {
@@ -19,9 +19,9 @@ class HomeController extends Controller
     /**
      * Show the application dashboard.
      *
-     * @return Response
+     * @return View
      */
-    public function index(): Response
+    public function index(): View
     {
         return view('home');
     }
