e7661f1e7c62cb8379758197f2204d4730addd86
Koen Hendriks <info@koenhendriks.com>
2018-05-27 12:10:38 +0200

    :bug: actually use oAuth

--- app/Http/Controllers/SMSController.php
+++ app/Http/Controllers/SMSController.php
@@ -32,7 +32,7 @@ class SMSController extends Controller
             CURLOPT_POSTFIELDS => $rawData,
             CURLOPT_URL => "https://api-prd.kpn.com/messaging/sms-kpn/v1/send",
             CURLOPT_HTTPHEADER => [
-                'Authorization: BearerToken '.env('KPN_SMS_KEY'),
+                'Authorization: BearerToken '.$this->oauth(),
                 'Content-Type: application/json',
                 'Content-Length: ' . strlen($rawData)
             ]
@@ -43,5 +43,29 @@ class SMSController extends Controller
         curl_close($curl);
 
         return collect(json_decode($response,true));
+    }
+
+    public function oauth()
+    {
+        $url = "https://api-prd.kpn.com/oauth/client_credential/accesstoken?grant_type=client_credentials";
+        $curl = curl_init();
+        curl_setopt_array($curl, [
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_POST => 1,
+            CURLOPT_POSTFIELDS => http_build_query([
+                'client_id' => env('KPN_SMS_ID'),
+                'client_secret' => env('KPN_SMS_KEY')
+            ]),
+            CURLOPT_URL => $url,
+            CURLOPT_HTTPHEADER => [
+                'Content-Type: application/x-www-form-urlencoded',
+            ]
+        ]);
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        return json_decode($response,1)['access_token'];
+
     }
 }
